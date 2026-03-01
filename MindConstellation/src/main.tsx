8d32735b870bd0c546d416747d8a007f41404077
Nikolaj Kim <nikolajkkim@gmail.com>
2026-02-28 20:30:54 -0800

    fixed main.tsx

--- MindConstellation/src/main.tsx
+++ MindConstellation/src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import App2 from './App2.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    <App2 />
+    <App />
   </StrictMode>,
 )
